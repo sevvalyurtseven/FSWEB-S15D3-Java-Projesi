0710f96181d866fb5db5a67cfdca694a67a95ef9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-02 21:22:08 +0300

    POJO olusturuldu

--- src/com/workintech/entity/Employee.java
+++ src/com/workintech/entity/Employee.java
@@ -4,4 +4,43 @@ public class Employee {
     private int id;
     private String firstName;
     private String lastName;
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
